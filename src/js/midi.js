bff0c60b2228d9af56047d8b84a91458b1783660
Kristoffer Ekstrand <kristoffer.ekstrand@gmail.com>
2025-12-05 23:45:30 +0000

    refactor: encapsulate MIDI properties and methods as private fields in MIDI class

--- src/js/midi.js
+++ src/js/midi.js
@@ -6,41 +6,43 @@ import appState from './app-state.js';
  * Supports hot-plug: devices can be connected/disconnected at runtime
  */
 class MIDI {
+	#midiAccess = null;
+	#connectedInputs = new Map();
+	#boundHandleMIDIMessage = this.#handleMIDIMessage.bind(this);
+
 	constructor() {
-		this.midiAccess = null;
-		this.connectedInputs = new Map();
-		this.init();
+		this.#init();
 	}
 
-	init() {
-		if (this.isSupported()) {
-			this.requestAccess();
+	#init() {
+		if (this.#isSupported()) {
+			this.#requestAccess();
 		} else {
 			console.log('WebMIDI is not supported in this browser.');
 		}
 	}
 
-	isSupported() {
+	#isSupported() {
 		return !!navigator.requestMIDIAccess;
 	}
 
-	async requestAccess() {
+	async #requestAccess() {
 		try {
-			this.midiAccess = await navigator.requestMIDIAccess();
-			this.onMIDISuccess(this.midiAccess);
+			this.#midiAccess = await navigator.requestMIDIAccess();
+			this.#onMIDISuccess(this.#midiAccess);
 		} catch (error) {
-			this.onMIDIFailure(error);
+			this.#onMIDIFailure(error);
 		}
 	}
 
-	onMIDISuccess(midiAccess) {
+	#onMIDISuccess(midiAccess) {
 		console.log('WebMIDI supported');
-		this.setupMIDIInputs(midiAccess);
-		this.setupStateChangeListener(midiAccess);
-		this.updateConnectionState();
+		this.#setupMIDIInputs(midiAccess);
+		this.#setupStateChangeListener(midiAccess);
+		this.#updateConnectionState();
 	}
 
-	onMIDIFailure(error) {
+	#onMIDIFailure(error) {
 		console.error('Failed to get MIDI access:', error);
 		appState.midiConnected = false;
 	}
@@ -48,77 +50,82 @@ class MIDI {
 	/**
 	 * Listen for MIDI device connect/disconnect events
 	 */
-	setupStateChangeListener(midiAccess) {
+	#setupStateChangeListener(midiAccess) {
 		midiAccess.onstatechange = event => {
-			const port = event.port;
+			const { port } = event;
 
 			if (port.type === 'input') {
 				if (port.state === 'connected') {
-					this.connectInput(port);
+					this.#connectInput(port);
 				} else if (port.state === 'disconnected') {
-					this.disconnectInput(port);
+					this.#disconnectInput(port);
 				}
 			}
 
-			this.updateConnectionState();
+			this.#updateConnectionState();
 		};
 	}
 
 	/**
 	 * Set up all currently available MIDI inputs
 	 */
-	setupMIDIInputs(midiAccess) {
+	#setupMIDIInputs(midiAccess) {
 		for (const input of midiAccess.inputs.values()) {
-			this.connectInput(input);
+			this.#connectInput(input);
 		}
 	}
 
 	/**
 	 * Connect a single MIDI input device
 	 */
-	connectInput(input) {
-		if (this.connectedInputs.has(input.id)) {
+	#connectInput(input) {
+		if (this.#connectedInputs.has(input.id)) {
 			return; // Already connected
 		}
 
-		input.onmidimessage = this.handleMIDIMessage.bind(this);
-		this.connectedInputs.set(input.id, input);
+		input.onmidimessage = this.#boundHandleMIDIMessage;
+		this.#connectedInputs.set(input.id, input);
 		console.log(`MIDI connected: ${input.name}`);
 	}
 
 	/**
 	 * Disconnect a single MIDI input device
 	 */
-	disconnectInput(input) {
-		if (!this.connectedInputs.has(input.id)) {
+	#disconnectInput(input) {
+		if (!this.#connectedInputs.has(input.id)) {
 			return; // Not connected
 		}
 
 		input.onmidimessage = null;
-		this.connectedInputs.delete(input.id);
+		this.#connectedInputs.delete(input.id);
 		console.log(`MIDI disconnected: ${input.name}`);
 	}
 
 	/**
 	 * Update app state based on connected devices
 	 */
-	updateConnectionState() {
-		appState.midiConnected = this.connectedInputs.size > 0;
+	#updateConnectionState() {
+		appState.midiConnected = this.#connectedInputs.size > 0;
 	}
 
 	/**
-	 * Get list of connected MIDI input names
+	 * Returns a list of names of currently connected MIDI input devices.
+	 *
+	 * @public
+	 * @returns {string[]} Array of connected MIDI input device names.
+	 * @description
+	 * This method is part of the public API contract. It provides external access
+	 * to the names of currently connected MIDI input devices, without exposing
+	 * internal state or device objects.
 	 */
 	getConnectedDevices() {
-		return Array.from(this.connectedInputs.values()).map(input => input.name);
+		return Array.from(this.#connectedInputs.values()).map(input => input.name);
 	}
 
-	handleMIDIMessage(message) {
-		const [status, data1, data2] = message.data;
+	#handleMIDIMessage(message) {
+		const [status, note, velocity] = message.data;
 		const command = status >> 4;
 		const channel = status & 0xf;
-		const note = data1;
-		const velocity = data2;
 
 		switch (command) {
 			case 9: // Note on
