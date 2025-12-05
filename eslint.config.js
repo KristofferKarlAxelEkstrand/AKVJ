fd1367b120aa9e652b07da1ceafcdc80814735d2
Kristoffer Ekstrand <kristoffer.ekstrand@gmail.com>
2025-12-05 20:43:41 +0000

    refactor: clean up animations JSON structure by removing redundant entries

--- eslint.config.js
+++ eslint.config.js
@@ -3,34 +3,34 @@ import prettier from 'eslint-config-prettier';
 import globals from 'globals';
 
 export default [
-js.configs.recommended,
-prettier,
-{
-languageOptions: {
-ecmaVersion: 'latest',
-sourceType: 'module',
-globals: {
-...globals.browser
-}
-},
-rules: {
-'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-'no-console': 'off',
-'prefer-const': 'error',
-'no-var': 'error',
-eqeqeq: ['error', 'always'],
-curly: ['error', 'all']
-}
-},
-{
-files: ['*.config.js', 'generateAnimationsJson.js'],
-languageOptions: {
-globals: {
-...globals.node
-}
-}
-},
-{
-ignores: ['dist/**', 'node_modules/**', 'src/public/**']
-}
+	js.configs.recommended,
+	prettier,
+	{
+		languageOptions: {
+			ecmaVersion: 'latest',
+			sourceType: 'module',
+			globals: {
+				...globals.browser
+			}
+		},
+		rules: {
+			'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+			'no-console': 'off',
+			'prefer-const': 'error',
+			'no-var': 'error',
+			eqeqeq: ['error', 'always'],
+			curly: ['error', 'all']
+		}
+	},
+	{
+		files: ['*.config.js', 'generateAnimationsJson.js'],
+		languageOptions: {
+			globals: {
+				...globals.node
+			}
+		}
+	},
+	{
+		ignores: ['dist/**', 'node_modules/**', 'src/public/**']
+	}
 ];
